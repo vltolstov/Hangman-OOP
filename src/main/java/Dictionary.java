fa22d6582de7e070712a65de9de268e6cd71bd6e
Vladimir Tolstov <tolstov@gmail.com>
2025-03-10 11:22:34 +0700

    add dictionary logics

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Dictionary {
 
-    //библиотека слов
-    //метод получения слова
+    private List<String> dictionary;
+    private Random random = new Random();
 
+    public Dictionary() {
+        dictionary = new ArrayList<String>();
+        readAllWords();
+    }
+
+    public void readAllWords() {
+        try {
+            dictionary = Files.readAllLines(Paths.get("src/main/resources/words.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getWord() {
+        int randomIndex = random.nextInt(dictionary.size());
+        return dictionary.get(randomIndex);
+    }
 }
