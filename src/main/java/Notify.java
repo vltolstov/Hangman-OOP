5b8c6d6fc26573811e202a540f98b8e0ac782f9f
Vladimir Tolstov <tolstov@gmail.com>
2025-03-10 12:41:50 +0700

    add game notify class logics

--- src/main/java/Notify.java
+++ src/main/java/Notify.java
@@ -1,6 +1,18 @@
 public class Notify {
 
-    // класс уведомлений
-    //
+    public void getMessage(String message) {
+        System.out.println(message);
+    }
 
+    public void gerError(Integer errorCount) {
+        System.out.println("Такой буквы в слове нет." + " " + "Осталось попыток: " + errorCount);
+    }
+
+    public void getCongratulations() {
+        System.out.println("Победа!");
+    }
+
+    public void getCondolences(String secretWord) {
+        System.out.println("Вы проиграли. Попробуйте еще раз. Загаданное слово: " + secretWord);
+    }
 }
