852f3777decff826d8e59a64849d71b4dc1aadd1
Vladimir Tolstov <tolstov@gmail.com>
2025-03-10 11:04:13 +0700

    start class structure

---
+++ src/main/java/GameField.java
@@ -0,0 +1,8 @@
+public class GameField {
+
+    private String gameField;
+
+    // метод сброса
+    // метод установки символа
+
+}
--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,5 +1,16 @@
 public class Hangman {
+
+    // private static String SECRET_WORD;
+    // куда то GAME_STATUS и END_GAME_STATUS
+    // ERRORS
+    // куда то записать неверные буквы
+
     public static void main(String[] args) {
+
+        //вызовы и основной цикл
+
         System.out.println("Hangman");
     }
+
+    // метод инициализации?
 }
