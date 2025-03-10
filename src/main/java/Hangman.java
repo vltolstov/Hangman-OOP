9130a8323cf8068423a4e346b94c05a23d75df94
Vladimir Tolstov <tolstov@gmail.com>
2025-03-10 15:28:12 +0700

    add initialization game method

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,16 +1,41 @@
+import java.util.Scanner;
+
 public class Hangman {
 
-    // private static String SECRET_WORD;
-    // куда то GAME_STATUS и END_GAME_STATUS
-    // ERRORS
-    // куда то записать неверные буквы
+    private static final String START_COMMAND = "старт";
+    static Scanner scanner = new Scanner(System.in);
+    private static boolean GAME_STATUS;
+    private static boolean END_GAME_STATUS;
 
     public static void main(String[] args) {
+        do {
+            initialization();
+            if (GAME_STATUS) {
 
-        //вызовы и основной цикл
+                Mistake mistake = new Mistake(6);
+                Dictionary dictionary = new Dictionary();
+                String secretWord = dictionary.getWord();
+                GameField gameField = new GameField(secretWord);
+                System.out.println(gameField.getGameField());
 
-        System.out.println("Hangman");
+//                while (!END_GAME_STATUS) {
+//                playerTurn();
+//                System.out.println(GAME_FIELD);
+//                checkGameStatus();
+//                }
+//                viewResult();
+            }
+        } while (GAME_STATUS);
     }
 
-    // метод инициализации?
+    public static void initialization() {
+        System.out.println("Для старта новой игры введите 'Старт'. Для отмены игры нажмите Enter: ");
+        String commandFromUserInput = scanner.nextLine().toLowerCase();
+        if (commandFromUserInput.equals(START_COMMAND)) {
+            GAME_STATUS = true;
+            END_GAME_STATUS = false;
+        } else {
+            GAME_STATUS = false;
+        }
+    }
 }
