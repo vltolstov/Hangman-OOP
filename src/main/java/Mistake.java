8e9716e04ae54a085054fdf4383d10e2b0d45398
Vladimir Tolstov <tolstov@gmail.com>
2025-03-10 14:45:11 +0700

    add mistake class logics

--- src/main/java/Mistake.java
+++ src/main/java/Mistake.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mistake {
 
-    // подсчет количества ошибок
-    // ошибки неправильных ходов
-    // запись неправильных-повторяющихся символов
+    private int mistakes;
+    private List<String> duplicateWrongLetters;
 
+    private Mistake() {
+        this.mistakes = 0;
+        this.duplicateWrongLetters = new ArrayList<>();
+    }
+
+    public void addMistake() {
+        this.mistakes += 1;
+    }
+
+    public int getMistakes() {
+        return this.mistakes;
+    }
+
+    public void setDuplicateWrongChars(Character letter) {
+        duplicateWrongLetters.add(Character.toString(letter));
+    }
 }
